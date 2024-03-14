3f951aca309c0ccb915874e222bc2887dc332399
Daniel Guerrero <daniel.guerrero@outlook.com>
2024-03-14 17:43:59 -0600

    Mi tercer cambio

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
-var MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/db_libreria'
+// var MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/db_libreria'
+
+var MONGO_URL = process.env.MONGO_URL || 'mongodb://trevor:N02M03D12@docdb-2024-03-14-23-26-01.cluster-c5qe8agegqzp.us-east-2.docdb.amazonaws.com:27017/db_libreria?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false'
 
 mongoose.connect(MONGO_URL);
 
